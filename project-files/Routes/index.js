f45661121be729f7b5deff484da49ade582f10b6
lamia-x5 <lamia-abdullah@outlook.com>
2022-06-16 11:53:47 +0300

    done

--- project-files/Routes/index.js
+++ project-files/Routes/index.js
@@ -1,5 +1,47 @@
 const router = require('express').Router()
+const courseController =require('../Controller/course');
+const userController =require('../Controller/user');
+const cardController =require('../Controller/card');
+const commentsController =require('../Controller/comments');
 
-// your routes here
+//** course
+//insert-create
+router.post("/course/create",courseController.create);
+//find
+router.get("/courses",courseController.index);
+//update
+router.put("/course/update/",courseController.update);
+//Delete
+router.delete("/course/delete/",courseController.delete);
 
-module.exports = router
+//** card
+//insert-create
+router.post("/card/create",cardController.create);
+//find
+router.get("/card",cardController.index);
+//update
+router.put("/card/update/:id",cardController.update);
+//Delete
+router.delete("/card/delete/:id",cardController.delete);
+
+//** user
+//insert-create
+router.post("/user/create",userController.create);
+//find
+router.get("/user",userController.index);
+//update
+router.put("/user/update/:name",userController.update);
+//Delete
+router.delete("/user/delete/:id",userController.delete);
+
+//** comment
+//insert-create
+router.post("/comments/create",commentsController.create);
+//find
+router.get("/comments",commentsController.index);
+//update
+router.put("/comments/update/:id",commentsController.update);
+//Delete
+router.delete("/comments/delete/:id",commentsController.Delete);
+
+module.exports = router;
